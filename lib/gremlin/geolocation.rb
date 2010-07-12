27ed892efe561cf69159ddb0f16b4d8c93016f81
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-12 02:59:20 +0100

    added fake data generators

---
+++ lib/gremlin/geolocation.rb
@@ -0,0 +1,11 @@
+module Gremlin
+  module Geolocation
+    def lat
+      (rand(1.8e11)-9e10)/1e9 # precision 9
+    end
+
+    def lng
+      (rand(3.6e11)-1.8e11)/1e9 # precision 9
+    end
+  end
+end
---
+++ lib/gremlin/internet.rb
@@ -0,0 +1,42 @@
+module Gremlin
+  module Internet
+
+    def email
+      [ user_name(name), domain_name ].join('@')
+    end
+
+    def free_email
+      "#{user_name(name)}@#{HOSTS.rand}"
+    end
+
+    def user_name
+      case rand(2)
+      when 0
+        Name.first_name.gsub(/\W/, '').downcase
+      when 1
+        parts = [ Name.first_name, Name.last_name ].each {|n| n.gsub!(/\W/, '') }
+        parts = parts.join %w(. _)[Kernel.rand(2)].dup
+        parts.downcase!
+        parts
+      end
+    end
+
+    def domain_name
+      "#{domain_word}.#{domain_suffix}"
+    end
+
+    def domain_word
+      dw = Company.name.split(' ').first
+      dw.gsub!(/\W/, '')
+      dw.downcase!
+      dw
+    end
+
+    def domain_suffix
+      DOMAIN_SUFFIXES.rand
+    end
+
+    HOSTS = k %w(gmail.com yahoo.com hotmail.com)
+    DOMAIN_SUFFIXES = k %w(co.uk com us uk ca biz info name)
+  end
+end
