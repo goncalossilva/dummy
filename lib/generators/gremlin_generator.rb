a43825f2baff17d39a6798b317f825bfe3b0e224
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-14 04:07:59 +0100

    major generator overhaul: - Added an heuristic which estimates the amount of records to create for each model by using its associations with other models (mimicking real apps) - Code highly refactored and modularity improved

--- lib/generators/gremlin_generator.rb
+++ lib/generators/gremlin_generator.rb
@@ -1,11 +1,14 @@
 require 'rails/generators'
 require 'rubygems'
 require 'gremlin'
+require 'active_support'
 
 class GremlinGenerator < Rails::Generators::Base
   def self.source_root
     @source_root ||= File.expand_path('../templates', __FILE__)
   end
+  class_option :base_record_amount, :type => :numeric, :default => 10,
+              :desc => "The base amount of records to generate for each model. Depending on associations, the generated value may vary depending on the specified base amount."
   
   def install_gremlin
     initialize_application
@@ -21,21 +24,86 @@ class GremlinGenerator < Rails::Generators::Base
   end
   
   def generate_gremlin_data
-    Dir["app/models/*.rb"].each do |full_path|
-      name = File.basename(full_path).chomp(File.extname(full_path))
-      file = File.new("test/gremlin/#{name}.yml","w")
+    get_table_names
+    gather_associations
+    predict_record_amounts
+    generate_and_write_data
+  end
   
-      (0..10).each do |num|
+  def get_table_names
+    @models = Hash.new
+    Dir["app/models/*.rb"].each do |full_path|
+      model = File.basename(full_path).chomp(File.extname(full_path)).camelcase.constantize
+      @models.merge!({model => {
+        :record_amount => 0, :associations => []
+      }}) if model.respond_to?(:columns)
+    end
+  end
+  
+  def gather_associations
+    @models.each_key do |model|
+      associations = model.reflect_on_all_associations(:belongs_to)
+      
+      associations.each do |association|
+        if association.options.empty?
+          name = association.name.to_s
+          
+          @models[model][:associations].push({
+            :class_name  => name.camelcase,
+            :foreign_key => "#{name}_id"
+          })
+        elsif association.options.has_key?(:class_name)
+          @models[model][:associations].push(association.options)
+        end
+      end
+      
+    end
+  end
+  
+  def predict_record_amounts
+    models = @models.dup
+    models.each do |model, info|
+      predict_record_amount(model, info, models, [])
+    end
+  end
+  
+  def predict_record_amount(model, info, models, stacked_models)
+    info[:associations].each do |association|
+      assoc_model = association[:class_name].constantize
+      
+      if model != assoc_model and not stacked_models.include?(assoc_model)      
+        stacked_models << assoc_model
+        predict_record_amount(assoc_model, @models[assoc_model], models, stacked_models)
+      end
+    end
+    
+    if info[:associations].empty?
+      @models[model][:record_amount] = options.base_record_amount
+    else
+      amount = info[:associations].map do |association|
+        @models[association[:class_name].constantize][:record_amount]
+      end.max*2
+      
+      @models[model][:record_amount] = amount
+    end
+    stacked_models.delete(model)
+    models.delete(model)
+  end
+  
+  def generate_and_write_data
+    empty_directory "test/gremlin"
+    
+    @models.each do |model, info|
+      name = model.to_s.underscore
+      file = File.new("test/gremlin/#{name}.yml","w")
+      
+      (0..info[:record_amount]).each do |num|
         key_value = Hash.new
         fixture_data = Hash.new
         
-        begin
-          eval(name.camelcase).columns.each do |column|
-            key_value = generate_record_data(name, column)
-            fixture_data.merge!(key_value) unless key_value.nil?
-          end
-        rescue NoMethodError => e
-          puts "#{e}" # TODO: remove
+        model.columns.each do |column|
+          key_value = generate_record_data(name, column)
+          fixture_data.merge!(key_value) unless key_value.nil?
         end
         
         fixture = { "#{name}_#{num}" => fixture_data }
@@ -43,11 +111,10 @@ class GremlinGenerator < Rails::Generators::Base
       end
       say_status :successful, "data generation for '#{name}'"
       file.close
-    end 
+    end
   end
   
   def generate_record_data(name, column)
-    # Journal.reflect_on_all_associations(:belongs_to).count => 3 # FIXME
     if column.name.include? "_id" then
 			val = "@" + column.name.sub("_id","").pluralize + "[rand(@" + column.name.sub('_id','').pluralize + ".length)][1]"
 		else
@@ -65,9 +132,7 @@ class GremlinGenerator < Rails::Generators::Base
 					val = Gremlin::Lorem.paragraph
 				when 'integer' then
 					val = rand(1000)
-				when 'datetime' then
-					val = Date.today.to_s
-				when 'date' then
+				when 'date', 'datetime' then
 					val = Date.today.to_s
 				when 'decimal' then
 					val = rand(50).to_s + "." + (1000+rand(2000)).to_s
