40ad334aabf72970981b334b6dfedf062533ad71
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-16 07:26:54 +0100

    name tests included

---
+++ spec/name_spec.rb
@@ -0,0 +1,15 @@
+require "spec_helper"
+
+describe Gremlin::Name do
+  it "generates a valid first name" do
+    Gremlin::Name.first_name.should match /^[A-Z][a-z]+$/
+  end
+  
+  it "generates a valid last name" do
+    Gremlin::Name.last_name.should match /^[A-Z][a-z]+$/
+  end
+  
+  it "generates a valid name" do
+    Gremlin::Name.name.should match /^[A-Za-z\. ]*([A-Z][a-z ]+){2}[A-Za-z\. ]*$/
+  end
+end
