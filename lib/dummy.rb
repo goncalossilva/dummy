76906db8f464db0fa81f77320ee9e1e05e6c1271
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-25 03:51:50 +0100

    consistency++

--- lib/dummy.rb
+++ lib/dummy.rb
@@ -1,21 +1,21 @@
-require 'rubygems'
-require 'dummy/address'
-require 'dummy/company'
-require 'dummy/internet'
-require 'dummy/lorem'
-require 'dummy/name'
-require 'dummy/phone_number'
-require 'dummy/geolocation'
+require "rubygems"
+require "dummy/address"
+require "dummy/company"
+require "dummy/internet"
+require "dummy/lorem"
+require "dummy/name"
+require "dummy/phone_number"
+require "dummy/geolocation"
 
 module Dummy
   extend self
-  
+
   def numerify(number_string)
     number_string.gsub(/#/) { rand(10).to_s }
   end
 
   def letterify(letter_string)
-    letter_string.gsub(/\?/) { ('a'..'z').to_a.rand }
+    letter_string.gsub(/\?/) { ("a".."z").to_a.rand }
   end
 
   def bothify(string)
@@ -28,3 +28,4 @@ class Array
     self[super(self.length)]
   end
 end
+
