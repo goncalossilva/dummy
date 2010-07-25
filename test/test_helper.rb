e92f3425c73a036e4c3a1ca9aaebb7151d9221d8
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-25 03:53:52 +0100

    test_helper now aids generator testing

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,2 +1,14 @@
+require "rubygems"
 require "dummy"
+require "generators/dummy_generator"
 require "test/unit"
+require "rails/all"
+require "rails/generators"
+require "rails/generators/test_case"
+
+module Rails
+  def self.root
+    @root ||= File.expand_path(File.join(File.dirname(__FILE__), '..', 'tmp', 'rails_test'))
+  end
+end
+
