89299a76c8c6564eba8f8fa351260397a7d95fa6
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-26 19:30:54 +0100

    change ' with " for consistency

--- lib/dummy/address.rb
+++ lib/dummy/address.rb
@@ -26,7 +26,7 @@ module Dummy
     end
 
     def street_address
-      str = ( '#' * rand(3) ) << ('### %s' % street_name)
+      str = ( "#" * rand(3) ) << ("### %s" % street_name)
 
       Dummy.numerify(str)
     end
@@ -41,71 +41,71 @@ module Dummy
       STREET_SUFFIX.rand
     end
 
-    ZIP_FORMATS = ['#####', '#####-####']
+    ZIP_FORMATS = ["#####", "#####-####"]
 
-    STATE = ['Alabama', 'Alaska', 'Arizona', 'Arkansas',
-      'California', 'Colorado', 'Connecticut', 'Delaware', 'Florida',
-      'Georgia', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas',
-      'Kentucky', 'Louisiana', 'Maine', 'Maryland', 'Massachusetts',
-      'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
-      'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
-      'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon',
-      'Pennsylvania', 'Rhode Island', 'South Carolina', 'South Dakota',
-      'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virginia', 'Washington',
-      'West Virginia', 'Wisconsin', 'Wyoming']
+    STATE = ["Alabama", "Alaska", "Arizona", "Arkansas",
+      "California", "Colorado", "Connecticut", "Delaware", "Florida",
+      "Georgia", "Hawaii", "Idaho", "Illinois", "Indiana", "Iowa", "Kansas",
+      "Kentucky", "Louisiana", "Maine", "Maryland", "Massachusetts",
+      "Michigan", "Minnesota", "Mississippi", "Missouri", "Montana",
+      "Nebraska", "Nevada", "New Hampshire", "New Jersey", "New Mexico", "New York",
+      "North Carolina", "North Dakota", "Ohio", "Oklahoma", "Oregon",
+      "Pennsylvania", "Rhode Island", "South Carolina", "South Dakota",
+      "Tennessee", "Texas", "Utah", "Vermont", "Virginia", "Washington",
+      "West Virginia", "Wisconsin", "Wyoming"]
 
     STATE_ABBR = %w(AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA
                      KS KY LA ME MH MD MA MI MN MS MO MT NE NV NH NJ NM NY NC
                      ND MP OH OK OR PW PA PR RI SC SD TN TX UT VT VI VA WA WV
                      WI WY AE AA AP)
     
-    CITIES = ['New York', 'Los Angeles', 'Chicago', 'Houston', 'Phoenix', 'Philadelphia',
-      'San Antonio', 'San Diego', 'Dallas', 'San Jose', 'Detroit', 'San Francisco',
-      'Jacksonville', 'Indianapolis', 'Austin', 'Columbus', 'Fort Worth',
-      'Charlotte', 'Memphis', 'Boston', 'Baltimore', 'El Paso', 'Seattle', 'Denver',
-      'Nashville', 'Milwaukee', 'Washington', 'Las Vegas', 'Louisville', 'Portland',
-      'Oklahoma City', 'Tucson', 'Atlanta', 'Albuquerque', 'Kansas City', 'Fresno',
-      'Sacramento', 'Long Beach', 'Mesa', 'Omaha', 'Virginia Beach', 'Miami',
-      'Cleveland', 'Oakland', 'Raleigh', 'Colorado Springs', 'Tulsa', 'Minneapolis',
-      'Arlington', 'Honolulu', 'Wichita', 'St. Louis', 'New Orleans', 'Tampa', 'Santa Ana',
-      'Anaheim', 'Cincinnati', 'Aurora', 'Bakersfield', 'Toledo', 'Pittsburgh',
-      'Riverside', 'Lexington', 'Stockton', 'Corpus Christi', 'Anchorage', 'St. Paul', 
-      'Newark', 'Plano', 'Buffalo', 'Henderson', 'Fort Wayne', 'Chandler',
-      'Greensboro', 'Lincoln', 'Glendale', 'St. Petersburg', 'Jersey City',
-      'Scottsdale', 'Orlando', 'Madison', 'Norfolk', 'Birmingham', 'Winston-Salem',
-      'Durham', 'Laredo', 'Lubbock', 'Baton Rouge', 'North Las Vegas', 'Chula Vista',
-      'Chesapeake', 'Garland', 'Reno', 'Hialeah', 'Arlington', 'Gilbert', 'Irvine',
-      'Rochester', 'Akron', 'Boise', 'Irving', 'Fremont', 'Richmond', 'Spokane',
-      'Modesto', 'Montgomery', 'Yonkers', 'Tacoma', 'Shreveport', 'Des Moines', 'San Bernardino', 
-      'Fayetteville', 'Glendale', 'Augusta', 'Grand Rapids', 'Huntington Beach',
-      'Newport News', 'Mobile', 'Little Rock', 'Moreno Valley', 'Columbus',
-      'Amarillo', 'Fontana', 'Oxnard', 'Knoxville', 'Fort Lauderdale', 'Salt Lake City',
-      'Worcester', 'Huntsville', 'Brownsville', 'Jackson', 'Overland Park',
-      'Tempe', 'Aurora', 'Oceanside', 'Tallahassee', 'Providence', 'Rancho Cucamonga',
-      'Ontario', 'Chattanooga', 'Santa Clarita', 'Garden Grove',
-      'Vancouver', 'Grand Prairie', 'Peoria', 'Sioux Falls', 'Springfield', 'Santa Rosa',
-      'Rockford', 'Salem', 'Springfield', 'Port St. Lucie', 'Cape Coral',
-      'Dayton', 'Eugene', 'Pomona', 'Corona', 'Alexandria', 'Joliet', 'Pembroke Pines',
-      'Paterson', 'Pasadena', 'Lancaster', 'Hayward', 'Salinas', 'Hampton',
-      'Palmdale', 'Pasadena', 'Naperville', 'Kansas City', 'Hollywood', 'Lakewood',
-      'Torrance', 'Escondido', 'Fort Collins', 'Syracuse', 'Bridgeport', 'Orange',
-      'Cary', 'Elk Grove', 'Savannah', 'Sunnyvale', 'Warren', 'Mesquite',
-      'Fullerton', 'McAllen', 'Columbia', 'Carrollton', 'Cedar Rapids', 'McKinney',
-      'Sterling Heights', 'Bellevue', 'Coral Springs', 'Waco', 'Elizabeth', 'West Valley City',
-      'Clarksville', 'Topeka', 'Hartford', 'Thousand Oaks', 'New Haven', 
-      'Denton', 'Concord', 'Visalia', 'Olathe', 'El Monte', 'Independence',
-      'Stamford', 'Simi Valley', 'Provo', 'Killeen', 'Springfield', 'Abilene',
-      'Thornton', 'Gainesville', 'Evansville', 'Roseville', 'Charleston', 'Peoria',
-      'Athens', 'Lafayette', 'Vallejo', 'Lansing', 'Ann Arbor', 'Inglewood', 'Santa Clara',
-      'Flint', 'Victorville', 'Costa Mesa', 'Beaumont', 'Manchester', 'Miami Gardens',
-      'Miramar', 'Norman', 'Westminster', 'Cambridge', 'Midland', 'Arvada',
-      'Allentown', 'Elgin', 'Waterbury', 'Downey', 'Clearwater', 'Billings', 'West Covina',
-      'Round Rock', 'Murfreesboro', 'Lewisville', 'West Jordan', 'Pueblo',
-      'San Buenaventura', 'Lowell', 'South Bend', 'Fairfield', 'Erie', 'Rochester',
-      'High Point', 'Richardson', 'Richmond', 'Burbank', 'Berkeley', 'Pompano Beach',
-      'Norwalk', 'Frisco', 'Columbia', 'Gresham', 'Daly City', 'Green Bay',
-      'Wilmington', 'Wichita Falls', 'Davenport', 'Antioch', 'Palm Bay',
-      'Centennial', 'Odessa', 'Boulder']
+    CITIES = ["New York", "Los Angeles", "Chicago", "Houston", "Phoenix", "Philadelphia",
+      "San Antonio", "San Diego", "Dallas", "San Jose", "Detroit", "San Francisco",
+      "Jacksonville", "Indianapolis", "Austin", "Columbus", "Fort Worth",
+      "Charlotte", "Memphis", "Boston", "Baltimore", "El Paso", "Seattle", "Denver",
+      "Nashville", "Milwaukee", "Washington", "Las Vegas", "Louisville", "Portland",
+      "Oklahoma City", "Tucson", "Atlanta", "Albuquerque", "Kansas City", "Fresno",
+      "Sacramento", "Long Beach", "Mesa", "Omaha", "Virginia Beach", "Miami",
+      "Cleveland", "Oakland", "Raleigh", "Colorado Springs", "Tulsa", "Minneapolis",
+      "Arlington", "Honolulu", "Wichita", "St. Louis", "New Orleans", "Tampa", "Santa Ana",
+      "Anaheim", "Cincinnati", "Aurora", "Bakersfield", "Toledo", "Pittsburgh",
+      "Riverside", "Lexington", "Stockton", "Corpus Christi", "Anchorage", "St. Paul", 
+      "Newark", "Plano", "Buffalo", "Henderson", "Fort Wayne", "Chandler",
+      "Greensboro", "Lincoln", "Glendale", "St. Petersburg", "Jersey City",
+      "Scottsdale", "Orlando", "Madison", "Norfolk", "Birmingham", "Winston-Salem",
+      "Durham", "Laredo", "Lubbock", "Baton Rouge", "North Las Vegas", "Chula Vista",
+      "Chesapeake", "Garland", "Reno", "Hialeah", "Arlington", "Gilbert", "Irvine",
+      "Rochester", "Akron", "Boise", "Irving", "Fremont", "Richmond", "Spokane",
+      "Modesto", "Montgomery", "Yonkers", "Tacoma", "Shreveport", "Des Moines", "San Bernardino", 
+      "Fayetteville", "Glendale", "Augusta", "Grand Rapids", "Huntington Beach",
+      "Newport News", "Mobile", "Little Rock", "Moreno Valley", "Columbus",
+      "Amarillo", "Fontana", "Oxnard", "Knoxville", "Fort Lauderdale", "Salt Lake City",
+      "Worcester", "Huntsville", "Brownsville", "Jackson", "Overland Park",
+      "Tempe", "Aurora", "Oceanside", "Tallahassee", "Providence", "Rancho Cucamonga",
+      "Ontario", "Chattanooga", "Santa Clarita", "Garden Grove",
+      "Vancouver", "Grand Prairie", "Peoria", "Sioux Falls", "Springfield", "Santa Rosa",
+      "Rockford", "Salem", "Springfield", "Port St. Lucie", "Cape Coral",
+      "Dayton", "Eugene", "Pomona", "Corona", "Alexandria", "Joliet", "Pembroke Pines",
+      "Paterson", "Pasadena", "Lancaster", "Hayward", "Salinas", "Hampton",
+      "Palmdale", "Pasadena", "Naperville", "Kansas City", "Hollywood", "Lakewood",
+      "Torrance", "Escondido", "Fort Collins", "Syracuse", "Bridgeport", "Orange",
+      "Cary", "Elk Grove", "Savannah", "Sunnyvale", "Warren", "Mesquite",
+      "Fullerton", "McAllen", "Columbia", "Carrollton", "Cedar Rapids", "McKinney",
+      "Sterling Heights", "Bellevue", "Coral Springs", "Waco", "Elizabeth", "West Valley City",
+      "Clarksville", "Topeka", "Hartford", "Thousand Oaks", "New Haven", 
+      "Denton", "Concord", "Visalia", "Olathe", "El Monte", "Independence",
+      "Stamford", "Simi Valley", "Provo", "Killeen", "Springfield", "Abilene",
+      "Thornton", "Gainesville", "Evansville", "Roseville", "Charleston", "Peoria",
+      "Athens", "Lafayette", "Vallejo", "Lansing", "Ann Arbor", "Inglewood", "Santa Clara",
+      "Flint", "Victorville", "Costa Mesa", "Beaumont", "Manchester", "Miami Gardens",
+      "Miramar", "Norman", "Westminster", "Cambridge", "Midland", "Arvada",
+      "Allentown", "Elgin", "Waterbury", "Downey", "Clearwater", "Billings", "West Covina",
+      "Round Rock", "Murfreesboro", "Lewisville", "West Jordan", "Pueblo",
+      "San Buenaventura", "Lowell", "South Bend", "Fairfield", "Erie", "Rochester",
+      "High Point", "Richardson", "Richmond", "Burbank", "Berkeley", "Pompano Beach",
+      "Norwalk", "Frisco", "Columbia", "Gresham", "Daly City", "Green Bay",
+      "Wilmington", "Wichita Falls", "Davenport", "Antioch", "Palm Bay",
+      "Centennial", "Odessa", "Boulder"]
 
     STREET_SUFFIX = %w(Alley Avenue Branch Bridge Brook Brooks
       Burg Burgs Bypass Camp Canyon Cape Causeway Center Centers Circle Circles
@@ -129,16 +129,16 @@ module Dummy
       Turnpike Underpass Union Unions Valley Valleys Via Viaduct View Views
       Village Villages Ville Vista Walk Walks Wall Way Ways Well Wells)
 
-    NEIGHBORHOOD = ['East of Telegraph Road', 'North Norridge', 'Northwest Midlothian/Midlothian Country Club',
-      'Mott Haven/Port Morris', 'Kingsbridge Heights', 'Bronxdale', 'Pennypack', 'Bridesburg',
-      'Allegheny West', 'Bushwick South', 'Dyker Heights', 'Ocean Parkway South', 'Summerlin North',
-      'Seven Hills Area', 'Greater Las Vegas National', 'phoenix', 'Central Chandler', 'South of Bell Road',
-      'River Heights', 'White Plains Central', 'Mount Kisco West', 'Pound Ridge East', 'Babylon Bayside',
-      'Sagaponack Seaside', 'South of Lake Ave', 'Far Rockaway/Bayswater', 'Jamaica Estates/Holliswood',
-      'Murray Hill', 'East Renton', 'Renton West', 'Auburn North', 'Northwoods West', 'Florissant West',
-      'Ladue South', 'Candlewood Country Club', 'West Covina East', 'North East Irwindale', 'Sunshine-Gardens',
-      'Cipriani', 'Brentwood Central', 'Jupiter South/Abacoa', 'Sea Ranch Lakes', 'Schall Circle/Lakeside Green',
-      'Olmsted Falls Central', 'South of Lake Shore Blvd', 'Gates Mills North', 'White Oak South of Columbia Pike',
-      'Rockville East of Hungerford Dr', 'Cleveland Park']
+    NEIGHBORHOOD = ["East of Telegraph Road", "North Norridge", "Northwest Midlothian/Midlothian Country Club",
+      "Mott Haven/Port Morris", "Kingsbridge Heights", "Bronxdale", "Pennypack", "Bridesburg",
+      "Allegheny West", "Bushwick South", "Dyker Heights", "Ocean Parkway South", "Summerlin North",
+      "Seven Hills Area", "Greater Las Vegas National", "phoenix", "Central Chandler", "South of Bell Road",
+      "River Heights", "White Plains Central", "Mount Kisco West", "Pound Ridge East", "Babylon Bayside",
+      "Sagaponack Seaside", "South of Lake Ave", "Far Rockaway/Bayswater", "Jamaica Estates/Holliswood",
+      "Murray Hill", "East Renton", "Renton West", "Auburn North", "Northwoods West", "Florissant West",
+      "Ladue South", "Candlewood Country Club", "West Covina East", "North East Irwindale", "Sunshine-Gardens",
+      "Cipriani", "Brentwood Central", "Jupiter South/Abacoa", "Sea Ranch Lakes", "Schall Circle/Lakeside Green",
+      "Olmsted Falls Central", "South of Lake Shore Blvd", "Gates Mills North", "White Oak South of Columbia Pike",
+      "Rockville East of Hungerford Dr", "Cleveland Park"]
   end
 end
--- lib/dummy/phone_number.rb
+++ lib/dummy/phone_number.rb
@@ -4,27 +4,31 @@ module Dummy
 
     def phone_number
       Dummy.numerify case rand(20)
-      when      0 then '###-###-#### x#####'
-      when      1 then '###-###-#### x####'
-      when      2 then '###-###-#### x###'
-      when   3..4 then '###-###-####'
-      when      5 then '###.###.#### x#####'
-      when      6 then '###.###.#### x####'
-      when      7 then '###.###.#### x###'
-      when   8..9 then '###.###.####'
-      when     10 then '(###)###-#### x#####'
-      when     11 then '(###)###-#### x####'
-      when     12 then '(###)###-#### x###'
-      when 13..14 then '(###)###-####'
-      when     15 then '1-###-###-#### x#####'
-      when     16 then '1-###-###-#### x####'
-      when     17 then '1-###-###-#### x###'
-      when 18..19 then '1-###-###-####'
+      when      0 then "###-###-#### x#####"
+      when      1 then "###-###-#### x####"
+      when      2 then "###-###-#### x###"
+      when   3..4 then "###-###-####"
+      when      5 then "###.###.#### x#####"
+      when      6 then "###.###.#### x####"
+      when      7 then "###.###.#### x###"
+      when   8..9 then "###.###.####"
+      when     10 then "(###)###-#### x#####"
+      when     11 then "(###)###-#### x####"
+      when     12 then "(###)###-#### x###"
+      when 13..14 then "(###)###-####"
+      when     15 then "1-###-###-#### x#####"
+      when     16 then "1-###-###-#### x####"
+      when     17 then "1-###-###-#### x###"
+      when 18..19 then "1-###-###-####"
       end
     end
 
     def short_phone_number
-      Dummy.numerify('###-###-####')
+      Dummy.numerify("###-###-####")
+    end
+    
+    def numeric_phone_number
+      Dummy.numerify("##########").to_i
     end
   end
 end
