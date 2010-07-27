f338f5a841892bab08ad3def5a4012fa4aed805f
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-27 19:11:34 +0100

    use VALIDATE to test company

--- test/company_test.rb
+++ test/company_test.rb
@@ -3,17 +3,15 @@ require File.expand_path(File.dirname(__FILE__) + "/test_helper")
 module DummyCompanyTests
   class DummyCompanyTest < Test::Unit::TestCase
     test "generates valid company names" do
-      assert_match /[ a-z]+/, Dummy::Company.name
+      assert_match VALIDATE[:company_name], Dummy::Company.name
     end
     
     test "generates valid company catch phrases" do
-      assert_match /[ a-z]+/, Dummy::Company.catch_phrase
-      assert_match /\s/, Dummy::Company.catch_phrase
+      assert_match VALIDATE[:company_catch_phrase], Dummy::Company.catch_phrase
     end
     
     test "generates valid corporate bullshit" do
-      assert_match /[ a-z]+/, Dummy::Company.bs
-      assert_match /\s/, Dummy::Company.bs
+      assert_match VALIDATE[:company_bs], Dummy::Company.bs
     end
   end
 end
