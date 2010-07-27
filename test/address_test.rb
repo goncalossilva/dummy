e37d6b16580669338e8d66e6b4601fb329aa997c
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-27 14:19:54 +0100

    added more tests for Dummy::Address

--- test/address_test.rb
+++ test/address_test.rb
@@ -5,5 +5,21 @@ module DummyAddressTests
     test "generates valid US zip codes" do
       assert_match /^([0-9]{5}-[0-9]{4}|[0-9]{5})$/, Dummy::Address.zip_code
     end
+    
+    test "generates valid US state" do
+      assert_match /^([A-Z][a-z]+)+$/, Dummy::Address.us_state
+    end
+    
+    test "generates valid short name for US state" do
+      assert_match /^[A-Z]{2}$/, Dummy::Address.us_state_short
+    end
+    
+    test "generates a valid street name" do
+      assert_match /^[A-Z][a-z"]+ [A-Z][a-z]+$/, Dummy::Address.street_name
+    end
+    
+    test "generates a valid street address" do
+      assert_match /^\d+ [A-Z][a-z"]+ [A-Z][a-z]+$/, Dummy::Address.street_address
+    end
   end
 end
