90ab5419e2381793da632a6d36ae6c0f6d4c78d8
Gonçalo Silva <goncalossilva@gmail.com>
2010-07-28 23:43:59 +0100

    added real tests for the rails generator

--- test/generators/dummy_generator_test.rb
+++ test/generators/dummy_generator_test.rb
@@ -1,17 +1,52 @@
 require File.expand_path(File.dirname(__FILE__) + "/../test_helper")
-require "rails/generators/rails/app/app_generator"
-require "rails/generators/rails/model/model_generator"
 
 class DummyGeneratorTest < Rails::Generators::TestCase
   destination File.join(Rails.root)
   tests Dummy::DataGenerator
   arguments []
 
-  setup :prepare_destination
   setup :create_test_application
+  teardown :clean_tmp
 
-  test 'Dummy data is generated' do    
+  test "dummy generates files with correct amount using default and user-defined options" do
     run_generator
+    
+    ["children", "parents", "grand_parents"].each do |model|
+      assert_file "test/dummy/#{model}.yml"
+    end
+    
+    all_options = {%w()    => [{:n => "children", :a => "40"}, 
+                              {:n => "parents", :a => "20"}, 
+                              {:n => "grand_parents", :a => "10"}],
+    %w(--growth-ratio=3.0) => [{:n => "children", :a => "90"}, 
+                              {:n => "parents", :a => "30"}, 
+                              {:n => "grand_parents", :a => "10"}],
+    %w(--growth-ratio=1.0) => [{:n => "children", :a => "10"}, 
+                              {:n => "parents", :a => "10"}, 
+                              {:n => "grand_parents", :a => "10"}],
+    %w(--base-amount=5)    => [{:n => "children", :a => "20"}, 
+                              {:n => "parents", :a => "10"}, 
+                              {:n => "grand_parents", :a => "5"}],
+    %w(--growth-factor=4.0 --base-amount=2) =>  
+                              [{:n => "children", :a => "8"}, 
+                              {:n => "parents", :a => "4"}, 
+                              {:n => "grand_parents", :a => "2"}]}
+    
+    all_options.each do |options, results|
+      clean_dummy
+      run_generator options
+      
+      results.each do |data|
+        header_line = File.read("test/dummy/#{data[:n]}.yml").match(/([^\n]+)/)[0]
+        assert_nil header_line.index("\n")
+        
+        regex = /# '(\w+)' data generated automatically by dummy at .+ \((\d+) records\)\./
+        results = header_line.match(regex)
+
+        assert_equal data[:n], results[1]
+        assert_equal data[:a], results[2]
+      end
+    end
   end
 end
 
