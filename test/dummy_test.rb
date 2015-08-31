e1c8f4e0901052cfd677d2e5561d44e4302abd37
steenzout <steenzout@ymail.com>
2015-08-31 16:45:02 -0600

    added explicit test failure message.

--- test/dummy_test.rb
+++ test/dummy_test.rb
@@ -7,7 +7,10 @@ module DummyTests
         data[:names].each do |name|
           class_eval <<-EOV
             def test_magic_generation_of_#{test.to_s}_for_#{type}_column_named_#{name}
-              assert_match VALIDATE[:#{test}], Dummy.magic_data("#{name}", :#{type}).to_s
+              assert_match(
+                VALIDATE[:#{test}],
+                Dummy.magic_data("#{name}", :#{type}).to_s,
+                "\tFailed magic generation of #{test.to_s} for column #{name} of type #{type}")
             end
           EOV
         end
